83b0921ed3bd739ef9f9136588c89685da8a3bff
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-03-25 00:20:50 -0300

    modified:   2.Results-Summary-Component-main/src/index.html 	modified:   2.Results-Summary-Component-main/src/script/script.js

--- 2.Results-Summary-Component-main/src/script/script.js
+++ 2.Results-Summary-Component-main/src/script/script.js
@@ -1,8 +1,15 @@
 import myJson from "../data.json" assert { type: "json" };
-var 
+var number_principal = document.getElementById("number_principal");
+var numberTag1 = document.getElementById("numberTag1");
+var numberTag2 = document.getElementById("numberTag2");
+var numberTag3 = document.getElementById("numberTag3");
+var numberTag4 = document.getElementById("numberTag4");
 
-var data = myJson.map(function (element) {
-  return element;
+console.log(myJson);
+number_principal.innerHTML = "Teste";
 
-});
-console.log(data);
+numberTag1.innerHTML = myJson[0].score;
+numberTag2.innerHTML = myJson[1].score;
+numberTag3.innerHTML = myJson[2].score;
+numberTag4.innerHTML = myJson[3].score;
+
